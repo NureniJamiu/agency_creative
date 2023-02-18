a3998eda751192f5e13e01bbe5df41dedfe1db64
abiton198 <abitonp@gmail.com>
2023-02-18 12:15:18 +0200

    Updated: Intro and About completed

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div>
+    <div className="bg-black text-[#F5F5F5]">
       <Navbar />
       <Intro/>
       <About/>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
        <img src={logo}/>
           <div className="flex justify-between gap-3">
             {/* hamburger for navbar */}
-            <nav class="container flex justify-between px-4 py-8 mx-auto bg-white">
+            <nav class="container flex justify-between px-4 py-8 mx-auto bg-black">
                
                <div class="hidden space-x-8 lg:flex">
                   <Link to="/">Home</Link>
@@ -19,16 +19,17 @@ const Navbar = () => {
 
                   <div class="flex lg:hidden">
                      <div class="space-y-2">
-                        <span class="block w-8 h-0.5 bg-gray-600 animate-pulse"></span>
-                           <span class="block w-6 h-0.5 bg-gray-600 animate-pulse ml-1"></span>
-                             <span class="block w-8 h-0.5 bg-gray-600 animate-pulse"></span>
+                        <span class="block w-8 h-0.5 bg-gray-300 animate-pulse"></span>
+                           <span class="block w-6 h-0.5 bg-gray-300 animate-pulse ml-1"></span>
+                             <span class="block w-8 h-0.5 bg-gray-300 animate-pulse"></span>
                                 </div>
                                     </div>
             </nav>
-
       </div>
     </div>
   );
 };
 
 export default Navbar;
+// Need attention:
+// ! links not active yet, needs a route
