5cb80b3c421ecf14d6b1a673d5a3ed26b66a3812
NureniJamiu <nurenijamiu951@gmail.com>
2023-02-18 21:28:13 +0100

    refactor: make basic code refactorings

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -9,19 +9,18 @@ const Intro = () => {
           Make your dream <br />
           business goal come true
         </h1>
-        <p className="text-sm pb-10">
-          {" "}
-          when you need us for improve your business,
-          <br />
-          then come with us to help your business have reach it,
-          <br />
-          you just sit and feel that goal.
+        <p className="text-md mb-10 w-96 mx-auto text-gray-300">
+          {/* removed <br/> tags and instead set a width of 24rem (w-96) and a "mx-auto" to center the paragraph element horizontally. Doing it this way will help a lot when it comes to making it responsive*/}
+          when you need us for improve your business, then come with us to help
+          your business have reach it, you just sit and feel that goal.
         </p>
-        <button className="bg-[#377DFF] text-sm rounded-3xl py-2.5 px-2.5 justify-center w-36">
+
+        {/* Changed the button to have a background set to white and blue when hovered on and also change the text colors respectively, and finally a little bit of transition to spice it up */}
+        <button className="bg-white text-black text-sm rounded-3xl py-2.5 px-2.5 justify-center w-36 hover:bg-[#377DFF] hover:text-white transition-all duration-200">
           Start Project
         </button>
 
-        {/* image - 1st in the page */}
+        {/* Image - 1st in the page */}
         <div className="align-middle mt-5">
           <img src={intro_img} />
         </div>
