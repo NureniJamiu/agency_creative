0bd7a3713afa0e724e7ef1eff0b8b1366c7f6b78
NureniJamiu <nurenijamiu951@gmail.com>
2023-02-18 21:10:28 +0100

    fix: replace span tags with actual menu icon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,27 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import logo from "../Images/logo.png";
+import { FiMenu } from "react-icons/fi";
 
 const Navbar = () => {
   return (
-    <div className="flex justify-between items-center p-[25px]">
+    <div className="flex justify-between items-center px-[25px]">
       <img src={logo} />
       <div className="flex justify-between gap-3">
         {/* hamburger for navbar */}
         <nav class="container flex justify-between px-4 py-8 mx-auto bg-black">
-          <div class="hidden space-x-8 lg:flex">
+          <div class="hidden space-x-8 md:flex">
             <Link to="/">Home</Link>
             <Link to="/About">About</Link>
             <Link to="/Action">Services</Link>
             <Link to="/Contact">Projects</Link>
           </div>
 
-          <div class="flex lg:hidden">
-            <div class="space-y-2">
-              <span class="block w-8 h-0.5 bg-gray-300 animate-pulse"></span>
-              <span class="block w-6 h-0.5 bg-gray-300 animate-pulse ml-1"></span>
-              <span class="block w-8 h-0.5 bg-gray-300 animate-pulse"></span>
-            </div>
+          <div class="flex md:hidden">
+            <FiMenu className="text-3xl" />
           </div>
         </nav>
       </div>
