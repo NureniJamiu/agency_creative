d542a3beae756daf585d9ce7f66d45f0662608b2
NureniJamiu <nurenijamiu951@gmail.com>
2023-02-21 21:49:54 +0100

    implement responsive service section - all screens

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -11,41 +11,50 @@ import { RiAdvertisementLine } from "react-icons/ri";
 
 const Services = () => {
   return (
-    <div className="flex-col pt-3 mx-5 text-justify justify-center">
+    <div className="pt-3 mx-5 md:mx-10 lg:mx-28 text-justify justify-center">
       <SectionTitle
         sectionName="Our Services"
         sectionDesc="Perfect and Fast Movement"
       />
-      <p className="text-[15px] text-gray-300 mb-5 text-center">
-        We move with make a Creative Strategy for help your business goal, we
-        help to improve your income by a services we have. make your content
-        look interesting and make people look for your business
-      </p>
-      <div className="text-white font-semibold">
-        <Link to="#">
-          <span className="text-sm flex items-center gap-2 transition duration-200 hover:translate-x-2 hover:text-blue-400">
-            Read more <BsArrowRight className="text-lg" />
-          </span>
-        </Link>
-      </div>
-      <div className="flex flex-col my-10 justify-center items-center">
-        <ServiceCard
-          text="Social Media Management"
-          card_bg="#0B0F15"
-          icon={<CgScreen />}
-        />
+      <div className="flex flex-col md:flex-row md:items-center md:gap-20">
+        <div className="flex-1">
+          <p className="hidden md:block mb-5">Our Services</p>
+          <h3 className="hidden md:block w-56 text-2xl font-bold mb-5">
+            Perfect and Fast Movement
+          </h3>
+          <p className="text-[15px] text-gray-300 mb-5 text-center md:text-left md:w-96">
+            We move with make a Creative Strategy for help your business goal,
+            we help to improve your income by a services we have. make your
+            content look interesting and make people look for your business
+          </p>
+          <div className="text-white font-semibold">
+            <Link to="#">
+              <span className="text-sm flex items-center gap-2 transition duration-200 hover:translate-x-2 hover:text-blue-400">
+                Read more <BsArrowRight className="text-lg" />
+              </span>
+            </Link>
+          </div>
+        </div>
 
-        <ServiceCard
-          text="Search Engine Optimization"
-          icon={<FiSettings />}
-          icon_bg="#FF4646"
-        />
-        <ServiceCard text="Design" icon_bg="#8EEA77" icon={<ImPencil2 />} />
-        <ServiceCard
-          text="Ads"
-          icon={<RiAdvertisementLine />}
-          icon_bg="#FFB21A"
-        />
+        <div className="grid md:grid-cols-2 md:flex-1  my-10 md:my-0 justify-center items-center md:gap-10">
+          <ServiceCard
+            text="Social Media Management"
+            card_bg="#0B0F15"
+            icon={<CgScreen />}
+          />
+
+          <ServiceCard
+            text="Search Engine Optimization"
+            icon={<FiSettings />}
+            icon_bg="#FF4646"
+          />
+          <ServiceCard text="Design" icon_bg="#8EEA77" icon={<ImPencil2 />} />
+          <ServiceCard
+            text="Ads"
+            icon={<RiAdvertisementLine />}
+            icon_bg="#FFB21A"
+          />
+        </div>
       </div>
     </div>
   );
