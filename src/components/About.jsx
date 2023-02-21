5627992e93641775c27879fd08ebf3f75c3ef62b
NureniJamiu <nurenijamiu951@gmail.com>
2023-02-21 21:49:00 +0100

    implement responsive about section for all screens

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,9 +7,9 @@ export default function About() {
   return (
     <div className="">
       <SectionTitle sectionName="About Us" sectionDesc="Our Teammate" />
-      <div className="flex flex-col justify-center mx-5 md:mx-10 lg:mx-20 md:flex-row-reverse md:gap-5 md:items-center">
+      <div className="flex flex-col justify-center mx-5 md:mx-10 lg:mx-28 md:flex-row-reverse md:gap-5 md:items-center">
         <div className="flex-1">
-          <div className="text-[15px] text-gray-300">
+          <div className="text-[15px] text-gray-300 md:w-[420px] md:float-right">
             <p>
               We move with make a Creative Strategy for help your business goal,
               we help to improve your income by a services we have. make your
@@ -23,7 +23,7 @@ export default function About() {
             </p>
           </div>
 
-          <div className="flex mt-8 justify-evenly">
+          <div className="flex mt-8 justify-evenly md:w-[420px] md:float-right">
             <button className="bg-[#E2E2E2] text-black text-sm rounded-3xl py-2.5 px-2.5 justify-center w-36">
               About us
             </button>
