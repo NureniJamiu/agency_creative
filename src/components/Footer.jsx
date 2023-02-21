6d9f2eeb1b3bafdd76189060e01fb8e276abc9ff
NureniJamiu <nurenijamiu951@gmail.com>
2023-02-21 22:53:24 +0100

    implement responsive footer section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,38 +9,40 @@ import {
 
 const Footer = () => {
   return (
-    <div className="py-10 px-5 bg-[#383638] ">
+    <div className="py-10 px-5 md:px-10 lg:px-28 bg-[#383638] ">
       <div className="mb-5">
         <img src={logo} alt="logo" className="w-32" />
       </div>
-      <div className="flex justify-between">
-        <div className="text-sm">
-          <p className=" font-bold text-base">Terms & Policies</p>
-          <p className="py-1 capitalize">Terms of services</p>
-          <p className="py-1 capitalize">Privacy Policy</p>
+      <div className="md:flex gap-28">
+        <div className="md:flex-1 flex justify-between">
+          <div className="text-sm">
+            <p className=" font-bold text-base">Terms & Policies</p>
+            <p className="py-1 capitalize">Terms of services</p>
+            <p className="py-1 capitalize">Privacy Policy</p>
+          </div>
+          <div className="text-sm">
+            <p className="font-bold text-base">Company</p>
+            <p className="py-1 capitalize">Home</p>
+            <p className="py-1 capitalize">About Us</p>
+            <p className="py-1 capitalize">Contact Us</p>
+          </div>
         </div>
-        <div className="text-sm">
-          <p className="font-bold text-base">Company</p>
-          <p className="py-1 capitalize">Home</p>
-          <p className="py-1 capitalize">About Us</p>
-          <p className="py-1 capitalize">Contact Us</p>
-        </div>
-      </div>
 
-      <div className="flex flex-col gap-10">
-        <div className="text-sm">
-          <p className=" font-bold text-base">Contact</p>
-          <p className="py-1 capitalize">(+123) 0011223344</p>
-          <p className="py-1 capitalize">agencycr@gmail.com</p>
-        </div>
-        <div className="text-sm">
-          <p className="font-bold text-base">Location</p>
-          <p className="py-1 capitalize">
-            PT Osiris Road Real Estate International
-          </p>
-          <p className="py-1 capitalize">JI, KH, Wahid Hasyim Kel No.10D</p>
-          <p className="py-1 capitalize">Jakarta, Indonesia</p>
-          <p className="py-1 capitalize">Jakarta, team@paircoding05.com</p>
+        <div className="md:flex-1 flex flex-col gap-10 md:flex-row">
+          <div className="text-sm">
+            <p className=" font-bold text-base">Contact</p>
+            <p className="py-1 capitalize">(+123) 0011223344</p>
+            <p className="py-1 capitalize">agencycr@gmail.com</p>
+          </div>
+          <div className="text-sm">
+            <p className="font-bold text-base">Location</p>
+            <p className="py-1 capitalize">
+              PT Osiris Road Real Estate International
+            </p>
+            <p className="py-1 capitalize">JI, KH, Wahid Hasyim Kel No.10D</p>
+            <p className="py-1 capitalize">Jakarta, Indonesia</p>
+            <p className="py-1 capitalize">Jakarta, team@paircoding05.com</p>
+          </div>
         </div>
       </div>
 
