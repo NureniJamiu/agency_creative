730662e4f64087eb0ef2d7b8b49ccc54a282177c
NureniJamiu <nurenijamiu951@gmail.com>
2023-02-21 22:04:57 +0100

    implement responsive portfolio section

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -11,20 +11,20 @@ const Portfolio = () => {
     { image: portfolio3, text: "Design Byte App" },
   ];
   return (
-    <div className="flex-col mx-5 text-justify justify-center">
+    <div className="flex-col mx-5 md:mx-10 lg:mx-28 text-justify justify-center">
       <SectionTitle sectionName="Our Portfolio" sectionDesc="What do we do" />
 
-      <p className="p-1 m-2 text-sm">
+      <p className="md:w-[70%] md:text-center text-gray-300 mx-auto">
         We move with make a Creative Strategy for help your business goal, we
         help to improve your income by a services we have. make your content
         look interesting and make people look for your business2
       </p>
-      {/* image - 2nd img in page */}
-      <div className="mt-5">
+
+      <div className="grid md:grid-cols-3 gap-5 my-8">
         {projects.map((project, index) => (
           <div
             key={index}
-            className="relative flex flex-col justify-center px align-center my-8"
+            className="relative flex flex-col justify-center px align-center hover:scale-105 transition-all duration-200"
           >
             <img src={project.image} alt={project.text} className="w-full" />
             <h3 className="absolute bottom-2 left-2 font-bold text-xl">
@@ -36,7 +36,7 @@ const Portfolio = () => {
 
       {/* buttons - about us */}
       <div className="flex m-3 justify-evenly">
-        <small className="bg-black border-2 border-gray-300 text-gray-300 text-base rounded-3xl py-2.5 font-bold px-10 justify-center">
+        <small className="bg-black border-2 border-gray-300 text-gray-300 text-base rounded-3xl py-2.5 font-bold px-10 justify-center hover:bg-gray-300 hover:text-black transition-all duration-200">
           See All Portfolio
         </small>
       </div>
