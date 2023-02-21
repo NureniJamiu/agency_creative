5074220c742f34be8dd5b6974281fd29698f5bfb
NureniJamiu <nurenijamiu951@gmail.com>
2023-02-21 21:01:46 +0100

    implement responsive intro section for all screens

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import intro_img from "../Images/intro_img.png";
 
 const Intro = () => {
@@ -9,26 +8,24 @@ const Intro = () => {
           Make your dream <br />
           business goal come true
         </h1>
-        <p className="text-[15px] mb-10 text-gray-300 px-5">
-          {/* removed <br/> tags and instead set a width of 24rem (w-96) and a "mx-auto" to center the paragraph element horizontally. Doing it this way will help a lot when it comes to making it responsive*/}
-          when you need us for improve your business, then come with us to help
-          your business have reach it, you just sit and feel that goal.
+        <p className="w-96 mx-auto text-[15px] mb-10 text-gray-300 px-5 md:w-[500px] md:mb-5">
+          when you need us to improve your business, then come to us. We'll help
+          your business reach that goal. You just sit back and feel it.
         </p>
 
-        {/* Changed the button to have a background set to white and blue when hovered on and also change the text colors respectively, and finally a little bit of transition to spice it up */}
         <button className="bg-white text-black text-sm rounded-3xl py-2.5 px-2.5 justify-center w-36 transition-all shadow-lg shadow-blue-300 duration-200 hover:translate-y-2 hover:shadow-md hover:shadow-blue-300">
           Start Project
         </button>
 
-        {/* Image - 1st in the page */}
-        <div className="align-middle mt-5">
-          {/* set a width of 100% to ensure that the image takes up the entire width of the device screen (mobile phones, tablets, and even large screens) - Helps in responsiveness of design */}
-          <img src={intro_img} className="w-full" />
+        <div className="align-middle mt-5 md:mt-1">
+          <img
+            src={intro_img}
+            className="w-full md:w-[450px] lg:w-[520px] mx-auto"
+          />
         </div>
       </div>
     </div>
   );
 };
 
-// ! image not responding to desktop alignment to centre
 export default Intro;
